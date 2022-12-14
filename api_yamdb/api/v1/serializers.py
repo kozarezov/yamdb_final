35121886530520e0b74129d57e90a6d68d6d5393
kozarezov <kozarezov.dd@gmail.com>
2022-12-14 15:04:25 +0300

    flake8-isort

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title
 
 User = get_user_model()
